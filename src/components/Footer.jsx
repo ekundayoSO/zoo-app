41c248d75980f15a70293dbf5d1cc7a748f1bded
Sulaimon Ekundayo <topdandydot@gmail.com>
2024-06-12 17:20:23 +0300

    Fix footer component to bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <div className="card-footer bg-dark text-light">
-      <footer className="text-center mt-4">
+    <div className="card-footer bg-dark text-light fixed-bottom">
+      <footer className="text-center m-2">
         <small>
-          <p className="p-3 mb-0">Copyright&copy; zoo app 2024</p>
+          <p className="p-2 mb-0">Copyright&copy; zoo app 2024</p>
         </small>
       </footer>
     </div>
