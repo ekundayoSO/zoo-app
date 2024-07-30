744a163ca4fd97da2425461e859507b91fe1d13f
Sulaimon Ekundayo <topdandydot@gmail.com>
2024-07-30 22:13:07 +0300

    Adjust footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="card-footer bg-dark text-light py-1 fixed-bottom">
+    <div className="bg-dark text-light py-1 fixed-bottom">
       <footer className="text-center m-2">
         <small>
           <p className="p-2 mb-0">Copyright&copy; zoo app 2024</p>
